900582f47d816ede4014bd5b80c3fdd32e79a21a
cuong <kutuzzrelex@gmail.com>
2018-06-25 19:59:48 +0700

    model

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -4,10 +4,12 @@ namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use App\Notifications\AdminResetPasswordNotification;
 
-class User extends Authenticatable
+class Admin extends Authenticatable
 {
     use Notifiable;
+    protected $guard = 'admin';
 
     /**
      * The attributes that are mass assignable.
@@ -15,7 +17,10 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name',
+        'email',
+        'password',
+        'job_title',
     ];
 
     /**
@@ -24,6 +29,12 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password',
+        'remember_token',
     ];
+
+    public function sendPasswordResetNotification($token)
+    {
+        $this->notify(new AdminResetPasswordNotification($token));
+    }
 }
